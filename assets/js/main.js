88b50e68d2f90647b8a226ed694d120f6ea99c4f
kbo580 <kobayuta1025@gmail.com>
2023-02-28 12:26:32 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,52 @@ $(function(){
     $(".menu").toggleClass('active');
   });
 
+  $('#toTop').on('click', function() {
+    $('html, body').animate({ 
+      scrollTop: 0 }, 300);
+  });
+
+  var swiper = new Swiper('.Mvswiper', {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
+    slidesPerView: '1',
+    spaceBetween: 30,
+    grabCursor: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+
+    breakpoints: {
+      768: {
+        spaceBetween: 150,
+        slidesPerView: 2.75,
+      }
+    },
+  });
+
+
+  var swiper = new Swiper('.styleswiper', {
+
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    grabCursor: true,
+    breakpoints: {
+      768: {
+        spaceBetween: 80,
+      }
+    },
+  });
 
 
 
